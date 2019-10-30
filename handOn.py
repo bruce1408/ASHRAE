e2ec3f06580399c7b1977e44a577763eb4477476
bruce1408 <summer56567@163.com>
2019-10-30 19:49:23 +0800

    ashrau_ubuntu

---
+++ handOn.py
@@ -0,0 +1,74 @@
+# coding=utf-8
+import pandas as pd
+import numpy as np
+import sklearn as sk
+from sklearn.model_selection import train_test_split
+from sklearn import linear_model, tree
+from sklearn.externals import joblib
+
+pd.set_option('display.max_columns', None)
+pd.set_option('display.width', 1000)
+pd.set_option('display.max_rows', None)
+
+# age and cabin is fullless
+dataPath = "/home/bruce/PycharmProjects/ashrae/data"
+df = pd.read_csv("./data/train.csv")
+withOutName = df.drop(["Name", "Cabin", "Ticket"], axis=1)  # 去掉年龄和住的房间号码
+print(withOutName.info())
+print(withOutName.describe())
+
+print(withOutName[withOutName.isnull().values==True].drop_duplicates())
+# print(withOutName[['Ticket', 'Pclass']].sort_values(by=['Pclass']))
+withOutName['Embarked'].fillna('S', inplace=True)
+print(withOutName.info())
+
+# 先预测年龄
+agePredictData = withOutName[withOutName['Age'].isnull()]
+ageData = withOutName[withOutName['Age'].notnull()]
+
+def extractInfo(ageData):
+
+    print(ageData.info())
+    ageDataSex = pd.get_dummies(ageData[['Sex']])
+    ageDataEmb = pd.get_dummies(ageData[['Embarked']])
+    ageData = ageData.join(ageDataSex)
+    ageData = ageData.join(ageDataEmb)
+    ageData = ageData.drop(["Sex", "Embarked"], axis=1)
+    print(ageData.info())
+    X = ageData[["Pclass", "Parch", "Fare", "Survived", "SibSp", "Embarked_C", "Embarked_Q", "Embarked_S", "Sex_male", "Sex_female"]]
+    Y = ageData["Survived"]
+    return X, Y
+
+# X, Y = extractInfo(ageData)
+# px, py = extractInfo(agePredictData)
+# # print(px)
+# trainX, testX, trainY, testY = train_test_split(X.values, Y.values, test_size=0.25, random_state=0)
+# # reg = linear_model.Ridge(alpha=0.5)
+# reg = linear_model.RidgeCV(alphas=[0.1, 1.0, 10.0])
+# reg.fit(trainX, trainY)
+# y_predict = reg.predict(px)
+# print(y_predict)
+# for i in range(len(y_predict)):
+#     if y_predict[i] < 0:
+#         y_predict[i] = y_predict[i] * -1
+# print(y_predict)
+# agePredictData["Age"] = y_predict
+# totalData = [agePredictData, ageData]
+# result = pd.concat(totalData)
+# print(result.info())
+# x, y = extractInfo(result)
+# print(x.values)
+# print(y.values)
+# train_x, test_x, train_y, test_y = train_test_split(x.values, y.values, test_size=0.1)
+# clf = tree.DecisionTreeClassifier()
+# clf.fit(train_x, train_y)
+# joblib.dump(clf, "train_model.m")
+# score = clf.score(test_x, test_y)
+# print("the result is: ", score)
+
+
+testCsvPath = "/home/bruce/PycharmProjects/ashrae/data/test.csv"
+testData = pd.read_csv(testCsvPath)
+print(testData.info())
+
+
